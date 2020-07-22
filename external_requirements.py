142c5476851246830b64529699d34d7a6ffb8318
Tony Hirst <tony.hirst@gmail.com>
2020-07-22 14:17:23 +0100

    Create external_requirements.py

---
+++ external_requirements.py
@@ -0,0 +1,7 @@
+# OU Utils
+git+https://github.com/innovationOUtside/nb_extension_wordexport.git
+git+https://github.com/innovationOUtside/nb_extension_odszip.git
+git+https://github.com/fschulze/sqlalchemy_schemadisplay.git
+git+https://github.com/innovationOUtside/ipython_magic_sqlalchemy_schemadisplay.git
+git+https://github.com/innovationOUtside/nb_workflow_tools.git
+git+https://github.com/innovationOUtside/tm351_utils.git
