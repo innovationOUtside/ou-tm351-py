99813d679e9c9caca18da67c9fc774af6b8294e1
Tony Hirst <tony.hirst@gmail.com>
2020-08-25 12:29:07 +0100

    Update external_requirements.py

--- external_requirements.py
+++ external_requirements.py
@@ -1,5 +1,5 @@
 # OU Utils
-git+https://github.com/innovationOUtside/nb_extension_wordexport.git
+# git+https://github.com/innovationOUtside/nb_extension_wordexport.git # replace with jupyter-docx-bundler
 git+https://github.com/innovationOUtside/nb_extension_odszip.git
 git+https://github.com/fschulze/sqlalchemy_schemadisplay.git
 git+https://github.com/innovationOUtside/ipython_magic_sqlalchemy_schemadisplay.git
